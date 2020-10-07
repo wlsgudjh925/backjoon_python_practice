fa4dbd507e2245811d7e9b3bc7f31ff3dcd03430
wlsgudjh925 <wlsgudjh0925@gmail.com>
2020-10-07 15:28:44 +0900

    알고리즘 스터디

---
+++ p10773.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+count = int(input())
+stack = []
+for i in range(0, count):
+    num = int(input())
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
