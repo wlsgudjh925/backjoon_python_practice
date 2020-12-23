1af0f703cb14a934688baba14a8c08c663dd75a6
wlsgudjh925 <wlsgudjh0925@gmail.com>
2020-12-23 19:38:31 +0900

    알고리즘 스터디

--- p1620.py
+++ p1620.py
@@ -2,18 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-dic = {}
+dic_name = []
+dic_num = {}
 
 for i in range(n):
-    poke = input()
-    dic[i+1] = poke
+    poke = input().rstrip()
+    dic_name.append(poke)
+    dic_num[poke] = i + 1
 
 for i in range(m):
-    qus = input()
-    try:
-        print(dic[int(qus)])
-    except:
-        for i in dic:
-            if dic[i] == qus:
-                print(i)
-
+    qus = input().rstrip()
+    if qus.isdigit():
+        print(dic_name[int(qus)-1])
+    else:
+        print(dic_num[qus])
